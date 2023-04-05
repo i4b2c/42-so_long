30e20ab0406ac1d3a98a74f4b6ad2fba6ea313e4
Ithalo Barbosa Caldas <icaldas@c1r5s8.42porto.com>
2023-04-05 16:24:14 +0100

    .

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:50:38 by marvin            #+#    #+#             */
-/*   Updated: 2023/02/28 13:50:38 by marvin           ###   ########.fr       */
+/*   Updated: 2023/04/05 16:21:23 by icaldas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,22 @@ int	verificar_len_linha(char *str)
 int	extra(char *str)
 {
 	int		fd;
-	char	s[1024];
+	char	*s;
 	int		i;
 
 	i = 0;
 	fd = open(str, O_RDONLY);
+	s = malloc(1024);
 	read(fd, s, 1024);
 	while (s[i])
 		i++;
 	if (s[i - 1] == '\n')
 	{
+		free (s);
 		close (fd);
 		return (0);
 	}
+	free (s);
 	close (fd);
 	return (1);
 }
